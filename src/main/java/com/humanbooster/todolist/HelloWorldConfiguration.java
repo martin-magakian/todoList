5a2260030b9dce7f26b9021f91ab1012bdb2e788
Martin Magakian <martin.magakian@gmail.com>
2017-09-02 12:43:58 +0200

    init

---
+++ src/main/java/com/humanbooster/todolist/HelloWorldConfiguration.java
@@ -0,0 +1,8 @@
+package com.humanbooster.todolist;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class HelloWorldConfiguration extends Configuration {
+}
---
+++ src/main/resources/com/humanbooster/todolist/person.ftl
@@ -0,0 +1,7 @@
+<#-- @ftlvariable name="" type="com.humanbooster.todolist.Person" -->
+<html>
+    <body>
+        <!-- calls getPerson().getName() and sanitizes it -->
+        <h1>Hello ${person.name?html}!</h1>
+    </body>
+</html>
