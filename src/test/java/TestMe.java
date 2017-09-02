0a05ea73a56b2206a01f3fddbc94b2f725cb939e
Martin Magakian <martin.magakian@gmail.com>
2017-09-02 14:38:52 +0200

    msg

---
+++ src/test/java/TestMe.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+/**
+ * Created by sochipan on 02/09/17.
+ */
+public class TestMe {
+
+    @Test
+    public void testMe(){
+        assertEquals(4, 2+2);
+    }
+
+}
